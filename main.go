72a08ff4af28b953fcf24642c1ab10199a3df7f9
NQHChuong <nguyenquochuychuong16112002@gmail.com>
2025-03-06 14:15:28 +0700

    Fix function for Login and Register

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"RestuarantBackend/db"
 	"RestuarantBackend/routers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,13 +17,21 @@ func main() {
 	// Initialize Router
 	router := gin.Default()
 
-	// // CORS Middleware
-	// router.Use(cors.New(cors.Config{
-	// 	AllowOrigins:     []string{"http://localhost:8080"},
-	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-	// 	AllowCredentials: true,
-	// }))
+	// CORS Middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+	//Xử lý OPTIONS request để tránh bị block bởi preflight request
+	router.OPTIONS("/*path", func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Status(204) // No Content
+	})
 
 	// Register User API Routes
 	routers.SetRoutesAPI(router)
